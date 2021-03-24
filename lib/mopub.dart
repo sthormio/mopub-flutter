728466dcea8d54354763697d7fd1d18cd33ee0bd
Bruno Alves <bruno.santos@ifpi.edu.br>
2021-03-24 12:20:55 -0300

    feat: call again init method if return false in first time

--- lib/mopub.dart
+++ lib/mopub.dart
@@ -7,14 +7,23 @@ class MoPub {
   static const MethodChannel _channel = const MethodChannel(MAIN_CHANNEL);
 
   static Future<bool> init(String adUnitId, {bool testMode = false}) async {
+    bool isAvailable = false;
+
     Map<String, dynamic> initValues = {
       "testMode": testMode,
       "adUnitId": adUnitId,
     };
 
     try {
-      final result = await _channel.invokeMethod(INIT_METHOD, initValues);
-      return result;
+      isAvailable = await _channel.invokeMethod(INIT_METHOD, initValues);
+
+      if (isAvailable) {
+        return isAvailable;
+      } else {
+        isAvailable = await _channel.invokeMethod(INIT_METHOD, initValues);
+      }
+
+      return isAvailable;
     } on PlatformException {
       throw PlatformException(
         message:
