772c454df02110320d8a4ff597cee025cf06d147
Bruno Alves <brukum2@gmail.com>
2021-04-28 11:47:38 -0300

    mopub ios

--- ios/Classes/SwiftFlutterMopubPlugin.swift
+++ ios/Classes/SwiftFlutterMopubPlugin.swift
@@ -3,7 +3,6 @@ import UIKit
 import MoPub
 
 public class SwiftMopubFlutterPlugin: NSObject, FlutterPlugin {
-  let isInitializedSdk:Bool = false
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: MopubConstants.MAIN_CHANNEL, binaryMessenger: registrar.messenger())
     registrar.addMethodCallDelegate(SwiftMopubFlutterPlugin(), channel: channel)
@@ -16,26 +15,22 @@ public class SwiftMopubFlutterPlugin: NSObject, FlutterPlugin {
     
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) -> Bool {
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     let args = call.arguments as? [String : Any] ?? [:]
     let testMode:Bool = args["testMode"] as? Bool ?? false
     let adUnitId:String = args["adUnitId"] as? String ?? ""
     switch call.method {
     case MopubConstants.INIT_METHOD:
         let sdkConfig = MPMoPubConfiguration(adUnitIdForAppInitialization: adUnitId)
-        sdkConfig.loggingLevel = testMode ? .debug : .debug
+        sdkConfig.loggingLevel = testMode ? .debug : .none
         
         MoPub.sharedInstance().initializeSdk(with: sdkConfig) {
             //SDK initialized
-            isInitializedSdk = true
-            
         }
         result(nil)
         break
     default:
         result(FlutterMethodNotImplemented)
     }
-
-    return isInitializedSdk
   }
 }
